9d484d5f0ec2bd019c160acbd3abcfb9b2e947ad
James Sully <sullyj3@gmail.com>
2014-12-11 22:13:59 +1100

    initial

---
+++ inputter.py
@@ -0,0 +1,52 @@
+def get_key_val_pairs():
+    another = True
+    d = dict()
+    while another:
+        print()
+        key = input('enter key:\n> ')
+        val = input('enter value:\n> ')
+
+        d[key] = val
+
+        print("another? ", end = "")
+        another = yesno(default = 'y')
+
+    return d
+
+def yesno(default = None):
+    response = None
+    responded = False
+
+    # what should the default answer be if the user hits return without typing
+    # anything?
+
+    if default:
+        if default == 'y':
+            y_n = '(Y/n)'
+        elif default == 'n':
+            y_n = '(y/N)'
+        else:
+            y_n = '(y/n)'
+    else:
+        y_n = '(y/n)'
+
+    while not responded:
+        response = input(y_n + "\n> ")
+        if not response:
+            if default:
+                if default == 'y':
+                    return True
+                else:
+                    return False
+            else:
+                print("please enter either 'y' or 'n'.")
+                continue
+
+        if not response in ('y','n'):
+            print("please enter either 'y' or 'n'.")
+        else:
+            if response == 'y':
+                return True
+            else:
+                return False
+
